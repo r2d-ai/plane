406dbb39a5290ba1ad9190f3d0b6af11e6b675b0
Huy Doan <tui@pm.me>
2026-09-25 17:49:37 +0700

    fix: show instance access tokens in admin sidebar

--- apps/admin/hooks/use-sidebar-menu/index.ts
+++ apps/admin/hooks/use-sidebar-menu/index.ts
@@ -14,6 +14,7 @@ export function useSidebarMenu(): TSidebarMenuItem[] {
     coreSidebarMenuLinks.email,
     coreSidebarMenuLinks.authentication,
     coreSidebarMenuLinks.workspace,
+    coreSidebarMenuLinks.access_tokens,
     coreSidebarMenuLinks.ai,
     coreSidebarMenuLinks.image,
   ];
